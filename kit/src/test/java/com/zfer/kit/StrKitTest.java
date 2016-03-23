f1a17e7a9ca9092e8ff34ef44e9b8f89bcfaf865
duololo <775901421@qq.com>
2016-03-23 13:12:08 +0800

    StrKit.java optimize

--- kit/src/test/java/com/zfer/kit/StrKitTest.java
+++ kit/src/test/java/com/zfer/kit/StrKitTest.java
@@ -1,5 +1,11 @@
 package com.zfer.kit;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 /**
@@ -9,6 +15,13 @@ import static org.junit.Assert.*;
  * @since 1.0
  */
 public class StrKitTest {
+    String strNull;
+    @Before
+    public void setUp(){
+        strNull = null;
+    }
+
+
     @Test
     public void testFirstCharToLowerCase(){
         String changedStr = StrKit.firstCharToLowerCase("klsdlD");
@@ -43,13 +56,12 @@ public class StrKitTest {
     public void testIsBlank(){
         assertEquals(true,StrKit.isBlank(" "));
         assertEquals(true,StrKit.isBlank(""));
-        String nullStr = null;
-        assertEquals(true,StrKit.isBlank(nullStr));
+        assertEquals(true,StrKit.isBlank((String)null));
 
         assertEquals(false,StrKit.isBlank("xx"));
 
         assertEquals(true,StrKit.isBlank("xx","","yy"));
-        assertEquals(true,StrKit.isBlank("xx",nullStr,"yy"));
+        assertEquals(true,StrKit.isBlank("xx",null,"yy"));
         assertEquals(false,StrKit.isBlank("xx","xx","yy"));
     }
 
@@ -57,17 +69,110 @@ public class StrKitTest {
     public void testNotBlank(){
         assertEquals(false,StrKit.notBlank(" "));
         assertEquals(false,StrKit.notBlank(""));
-        String nullStr = null;
-        assertEquals(false,StrKit.notBlank(nullStr));
+        assertEquals(false,StrKit.notBlank((String) null));
 
         assertEquals(true,StrKit.notBlank("xx"));
 
         assertEquals(false,StrKit.notBlank("xx","","yy"));
-        assertEquals(false,StrKit.notBlank("xx",nullStr,"yy"));
+        assertEquals(false,StrKit.notBlank("xx",strNull,"yy"));
         assertEquals(true,StrKit.notBlank("xx","xx","yy"));
     }
 
     @Test
-    public void testNotNull(){
+    public void testIsNull(){
+        assertEquals(false,StrKit.isNull(" "));
+        assertEquals(false,StrKit.isNull(""));
+        assertEquals(true,StrKit.isNull(strNull));
+
+        assertEquals(false,StrKit.isNull("xx"));
+
+        assertEquals(false,StrKit.isNull("xx","","yy"));
+        assertEquals(true,StrKit.isNull("xx",null,"yy"));
+        assertEquals(false,StrKit.isNull("xx","xx","yy"));
     }
+
+    @Test
+    public void testNotNull(){
+        assertEquals(true,StrKit.notNull(" "));
+        assertEquals(true,StrKit.notNull(""));
+        assertEquals(false,StrKit.notNull(strNull));
+
+        assertEquals(true,StrKit.notNull("xx"));
+
+        assertEquals(true,StrKit.notNull("xx","","yy"));
+        assertEquals(false,StrKit.notNull("xx",null,"yy"));
+        assertEquals(true,StrKit.notNull("xx","xx","yy"));
+    }
+
+    @Test
+    public void testToUtf8String(){
+        String str = StrKit.toUtf8String("中文");
+        assertEquals("%E4%B8%AD%E6%96%87",str);
+    }
+
+    @Test
+    public void testGetStr(){
+
+        assertEquals("",StrKit.getStr(null));
+        assertEquals("uuu",StrKit.getStr("uuu"));
+        assertEquals("uuu ",StrKit.getStr("uuu "));
+
+        assertEquals("yyy",StrKit.getStr(null,"yyy"));
+        assertEquals("yyy",StrKit.getStr("","yyy"));
+        assertEquals("ttt",StrKit.getStr("ttt","yyy"));
+
+        assertEquals("",StrKit.getStrAndTrim(null));
+        assertEquals("uuu",StrKit.getStrAndTrim("uuu"));
+        assertEquals("uuu",StrKit.getStrAndTrim("uuu "));
+
+        assertEquals("YY",StrKit.getStrAndTrim(" ","YY "));
+        assertEquals("YY",StrKit.getStrAndTrim(null,"YY "));
+    }
+
+    @Test
+    public void testReplace(){
+        String str1 = "select code,?,? from aa where num=? and code=?";
+        String str11 = StrKit.replace(str1, new String[]{"222","333","444","'555'"});
+        assertEquals("select code,222,333 from aa where num=444 and code='555'",str11);
+
+        String str2 = "select code,#,# from aa where num=# and code=#";
+        String str22 = StrKit.replace(str2,"#",new String[]{"222","333","444","'555'"});
+        assertEquals("select code,222,333 from aa where num=444 and code='555'",str22);
+    }
+
+    @Test
+    public void testContainsChineseChar(){
+        assertEquals(true,StrKit.containsChineseChar("ww我"));
+        assertEquals(false,StrKit.containsChineseChar("ww22#"));
+
+
+        assertEquals("xx",StrKit.getNotNullFirstStr("","","","xx","yyy"));
+        assertEquals("xx",StrKit.getNotNullFirstStr("","","","xx","yyy"));
+
+    }
+
+    @Test
+    public void testGetStrs(){
+        List<String> stringList = new ArrayList<String>();
+        stringList.add("x1");
+        stringList.add("x2");
+        stringList.add("x3");
+        stringList.add("x4");
+
+        assertEquals("x1;x2;x3;x4",StrKit.getStrs(stringList,";"));
+        assertEquals("x1-x2-x3-x4",StrKit.getStrs(stringList,"-"));
+    }
+
+    @Test
+    public void testGetStrByObj(){
+        java.util.Date date = new Date();
+        date.setTime(1458704405425L);
+
+        String str = StrKit.getStrByObj(date);
+        assertEquals("2016-03-23",str);
+
+
+        assertEquals("300.00",StrKit.getStrByObj(new BigDecimal("300.00"),"yyyy-MM-dd HH"));
+    }
+
 }
