403987c3169e192907e5591b1a23b8f0f9a03636
duololo <775901421@qq.com>
2016-03-24 13:30:29 +0800

    StrKit.java optimize

--- kit/src/main/java/com/zfer/kit/StrKit.java
+++ kit/src/main/java/com/zfer/kit/StrKit.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2011-2015
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,7 +26,8 @@ import java.util.regex.Pattern;
 
 /**
  * StrKit.
- * @author  yunshan
+ *
+ * @author yunshan
  * @version 1.0
  */
 public class StrKit {
@@ -39,91 +40,98 @@ public class StrKit {
     }
 
     /**
-	 * 首字母变小写
+     * 首字母变小写
      * <p>the first char to lower<br>
-	 * @param str the want to change string
-	 * @return return changed string
+     *
+     * @param str the want to change string
+     * @return return changed string
      */
-	public static String firstCharToLowerCase(String str) {
-		char firstChar = str.charAt(0);
-		if (firstChar >= 'A' && firstChar <= 'Z') {
-			char[] arr = str.toCharArray();
-			arr[0] += ('a' - 'A');
-			return new String(arr);
-		}
-		return str;
-	}
+    public static String firstCharToLowerCase(String str) {
+        char firstChar = str.charAt(0);
+        if (firstChar >= 'A' && firstChar <= 'Z') {
+            char[] arr = str.toCharArray();
+            arr[0] += ('a' - 'A');
+            return new String(arr);
+        }
+        return str;
+    }
 
     /**
      * 首字母变大写
      * <p>the first char to upper<br>
+     *
      * @param str the want to change string
      * @return return changed string
      */
-	public static String firstCharToUpperCase(String str) {
-		char firstChar = str.charAt(0);
-		if (firstChar >= 'a' && firstChar <= 'z') {
-			char[] arr = str.toCharArray();
-			arr[0] -= ('a' - 'A');
-			return new String(arr);
-		}
-		return str;
-	}
+    public static String firstCharToUpperCase(String str) {
+        char firstChar = str.charAt(0);
+        if (firstChar >= 'a' && firstChar <= 'z') {
+            char[] arr = str.toCharArray();
+            arr[0] -= ('a' - 'A');
+            return new String(arr);
+        }
+        return str;
+    }
 
     /**
      * 字符串为 null 或者为  "" 时返回 true
+     *
      * @param str judge words
      * @return if str is null or "" return true,else false
      */
-	public static boolean isBlank(String str) {
-		return StringUtils.isBlank(str);
-	}
-	
-	/**
-	 * 如果其中一个字符串为 null 或者为  "" 时返回 true
+    public static boolean isBlank(String str) {
+        return StringUtils.isBlank(str);
+    }
+
+    /**
+     * 如果其中一个字符串为 null 或者为  "" 时返回 true
+     *
      * @param strings judge words
      * @return if one str is null or "" return true,else false
-	 */
-	public static boolean isBlank(String... strings) {
-		if (strings == null) {
+     */
+    public static boolean isBlank(String... strings) {
+        if (strings == null) {
             return true;
         }
-		for (String str : strings) {
+        for (String str : strings) {
             if (isBlank(str)) {
                 return true;
             }
         }
-		return false;
-	}
-	
-	/**
-	 * 字符串不为 null 而且不为  "" 时返回 true
+        return false;
+    }
+
+    /**
+     * 字符串不为 null 而且不为  "" 时返回 true
+     *
      * @param str judge words
      * @return 字符串不为 null 而且不为  "" 时返回 true
-	 */
-	public static boolean notBlank(String str) {
-		return StringUtils.isNotBlank(str);
-	}
+     */
+    public static boolean notBlank(String str) {
+        return StringUtils.isNotBlank(str);
+    }
 
     /**
      * 所有字符串不为 null 而且不为  "" 时返回 true
+     *
      * @param strings judge words
      * @return 所有字符串不为 null 而且不为  "" 时返回 true
      */
-	public static boolean notBlank(String... strings) {
-		if (strings == null) {
-			return false;
-		}
-		for (String str : strings) {
+    public static boolean notBlank(String... strings) {
+        if (strings == null) {
+            return false;
+        }
+        for (String str : strings) {
             if (isBlank(str)) {
                 return false;
             }
         }
-		return true;
-	}
+        return true;
+    }
 
     /**
      * 所有对象为 null 时返回 true
+     *
      * @param paras judge objects
      * @return 所有对象为 null 时返回 true
      */
@@ -141,203 +149,215 @@ public class StrKit {
 
     /**
      * 所有对象不为 null 时返回 true
+     *
      * @param paras judge objects
      * @return 所有对象不为 null 时返回 true
      */
-	public static boolean notNull(Object... paras) {
-		if (paras == null) {
+    public static boolean notNull(Object... paras) {
+        if (paras == null) {
             return false;
         }
-		for (Object obj : paras) {
+        for (Object obj : paras) {
             if (obj == null) {
                 return false;
             }
         }
-		return true;
-	}
+        return true;
+    }
 
     /**
-     *  to utf8 string
-     *  js可以用decodeURI(msg)反向解码
+     * to utf8 string
+     * js可以用decodeURI(msg)反向解码
+     *
      * @param str want to change word
      * @return utf8 string of str
      */
-	public static String toUtf8String(String str) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < str.length(); i++) {
-			char c = str.charAt(i);
-			if (c <= 255) {
-				sb.append(c);
-			} else {
-				byte[] b;
-				try {
-					b = Character.toString(c).getBytes("utf-8");
-				} catch (UnsupportedEncodingException e) {
-					b = new byte[0];
-				}
+    public static String toUtf8String(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c <= 255) {
+                sb.append(c);
+            } else {
+                byte[] b;
+                try {
+                    b = Character.toString(c).getBytes("utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    b = new byte[0];
+                }
 
-				for (byte aB : b) {
+                for (byte aB : b) {
                     int k = aB;
                     if (k < 0) {
                         k += 256;
                     }
                     sb.append("%").append(Integer.toHexString(k).toUpperCase());
                 }
-			}
-		}
-		return sb.toString();
-	}
+            }
+        }
+        return sb.toString();
+    }
 
     /**
      * 获取字符串对象，为Null或者为空字符串,返回""
      * 可能会有空白字符产生
+     *
      * @param obj input param
      * @return string of obj
      */
-	public static String getStr(Object obj) {
-		return getStr(obj,"");
-	}
-	
-	/**
-	 * 获取字符串对象，为Null或者为空字符串,返回defaultString
+    public static String getStr(Object obj) {
+        return getStr(obj, "");
+    }
+
+    /**
+     * 获取字符串对象，为Null或者为空字符串,返回defaultString
      * 可能会有空白字符产生
+     *
      * @param obj,defaultStr input param
      * @return string of obj,if is blank return defaultStr
-	 */
-	public static String getStr(Object obj,String defaultStr){
-		return (obj == null || isBlank(obj.toString())) ? defaultStr : obj.toString();
-	}
+     */
+    public static String getStr(Object obj, String defaultStr) {
+        return (obj == null || isBlank(obj.toString())) ? defaultStr : obj.toString();
+    }
 
     /**
      * 获取字符串对象，为Null或者为空字符串,返回""，并且会trim
+     *
      * @param obj input param
      * @return string of obj,if is blank return defaultStr and trim
      */
-	public static String getStrAndTrim(Object obj){
-		return getStrAndTrim(obj,"");
-	}
+    public static String getStrAndTrim(Object obj) {
+        return getStrAndTrim(obj, "");
+    }
 
     /**
      * 获取字符串对象，为Null或者为空字符串,返回defaultString，并且会trim
+     *
      * @param obj,defaultStr param
      * @return string of obj,if is blank return defaultStr and trim
      */
-	public static String getStrAndTrim(Object obj,String defaultStr){
-		return (obj == null || isBlank(obj.toString())) ? defaultStr.trim() : obj.toString().trim();
-	}
+    public static String getStrAndTrim(Object obj, String defaultStr) {
+        return (obj == null || isBlank(obj.toString())) ? defaultStr.trim() : obj.toString().trim();
+    }
 
     /**
      * 多个相同的字符替换：比如将其中所有的问号，挨个换成数组内的值
+     *
      * @param str,fix,array input param
      * @return replace str width array[i] and append by fix
      */
-	public static String replace(String str,String fix,String[] array){
+    public static String replace(String str, String fix, String[] array) {
         String rs = str;
         for (String anArray : array) {
             rs = rs.replaceFirst(fix, anArray);
         }
-		return rs;
-	}
+        return rs;
+    }
 
     /**
      * 对所有的问号替换
+     *
      * @param str,array input param
      * @return replace str width array[i] and append by ?
      */
-	public static String replace(String str,String[] array){
-		String rs = str;
-		rs = StrKit.replace(rs,"\\?",array);
-		return rs;
-	}
-
+    public static String replace(String str, String[] array) {
+        String rs = str;
+        rs = StrKit.replace(rs, "\\?", array);
+        return rs;
+    }
 
 
     /**
      * 是否包含中文
+     *
      * @param str input param
      * @return if contains Chinese Char return true,else false
      */
-	public static boolean containsChineseChar(String str){
-       boolean temp = false;
-       Pattern p = Pattern.compile("[\u4e00-\u9fa5]"); 
-       Matcher m = p.matcher(str); 
-       if(m.find()){
-           temp = true;
-       }
-       return temp;
-	}
+    public static boolean containsChineseChar(String str) {
+        boolean temp = false;
+        Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
+        Matcher m = p.matcher(str);
+        if (m.find()) {
+            temp = true;
+        }
+        return temp;
+    }
 
     /**
      * 将所有对象中不为空的第一个字符串筛选出来，如果都为空，返回""
+     *
      * @param paras input param
      * @return first not null string
      */
-	public static String getNotNullFirstStr(String... paras) {
-		String rs = "";
-		if (paras == null){
-			return rs;
-		}
-		for (String str : paras){
-			if (notBlank(str)){
-				rs = str;
-				break;
-			}
-		}
-		return rs;
-	}
+    public static String getNotNullFirstStr(String... paras) {
+        String rs = "";
+        if (paras == null) {
+            return rs;
+        }
+        for (String str : paras) {
+            if (notBlank(str)) {
+                rs = str;
+                break;
+            }
+        }
+        return rs;
+    }
 
     /**
-     *  从List中 拼接字符串 fix做结合
+     * 从List中 拼接字符串 fix做结合
+     *
      * @param strList,fix input param
      * @return return string of list append by fix
      */
-	public static String getSplitStr(List<String> strList,String fix){
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<strList.size();i++){
-			String str = strList.get(i);
-			if(StrKit.notBlank(str)){
-				sb.append(str);
-				if(i+1 != strList.size()){
-					sb.append(fix);
-				}
-			}
-		}
-		return sb.toString();
-	}
+    public static String getSplitStr(List<String> strList, String fix) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strList.size(); i++) {
+            String str = strList.get(i);
+            if (StrKit.notBlank(str)) {
+                sb.append(str);
+                if (i + 1 != strList.size()) {
+                    sb.append(fix);
+                }
+            }
+        }
+        return sb.toString();
+    }
 
     /**
      * 增加一个方法，处理返回的对象，形成String
+     *
      * @param obj,dateFormat input param
      * @return return str by obj
      */
-	public static String getStrByObj(Object obj,String dateFormat){
-		String rs = "";
-		
-		if(obj == null){
-			return rs;
-		}
-		
-		if(obj instanceof BigDecimal){
-            BigDecimal bigDecimal = (BigDecimal)obj;
-            rs = bigDecimal.toString();
-		}else if(obj instanceof java.sql.Date
-				|| obj instanceof java.sql.Time
-				|| obj instanceof java.sql.Timestamp
-				|| obj instanceof java.util.Date){
-			rs = DateKit.toStr(DateKit.transDate(obj),dateFormat);
-		}else{
-			rs = obj.toString();
-		}
+    public static String getStrByObj(Object obj, String dateFormat) {
+        String rs = "";
 
-		return rs;
-	}
+        if (obj == null) {
+            return rs;
+        }
+
+        if (obj instanceof BigDecimal) {
+            BigDecimal bigDecimal = (BigDecimal) obj;
+            rs = bigDecimal.toString();
+        } else if (obj instanceof java.sql.Date
+                || obj instanceof java.sql.Time
+                || obj instanceof java.sql.Timestamp
+                || obj instanceof java.util.Date) {
+            rs = DateKit.toStr(DateKit.transDate(obj), dateFormat);
+        } else {
+            rs = obj.toString();
+        }
+
+        return rs;
+    }
 
     /**
      * 增加一个方法，处理返回的对象，形成String
+     *
      * @param obj input param
      * @return return str by obj
      */
-	public static String getStrByObj(Object obj){
-		return getStrByObj(obj,DateKit.dateFormat);
-	}
+    public static String getStrByObj(Object obj) {
+        return getStrByObj(obj, DateKit.dateFormat);
+    }
 }
